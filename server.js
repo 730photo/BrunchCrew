f7b54f38fd5bce95d544458925ffbd6a125b6dce
Reginald Lamar Bramber II <architekespy@Reginalds-MacBook-Pro.local>
2018-09-20 15:16:25 -0400

    heroku prep

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,8 +7,13 @@ var logger = require('morgan');
 const mongoose = require('mongoose')
 
 
-var indexRouter = require('./FanFinder/routes');
-var usersRouter = require('./FanFinder/routes/users');
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -20,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
