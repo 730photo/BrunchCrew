3fadc92be9d49496a3933d2154aeae6de936579d
Reginald Lamar Bramber II <architekespy@Reginalds-MacBook-Pro.local>
2018-09-21 09:53:23 -0400

    almost there

--- db/schema.js
+++ db/schema.js
@@ -1,2 +1,17 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    name: String,
+    age: Number,
+    city: String, 
+    Team: String
+})
+
+
+const UserModel = mongoose.model('user', UserSchema)
+
+
+module.exports = {
+    User: UserModel
+}
