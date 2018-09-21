b7c11ff2a0182084e27ddfe5fad45bc536ef269d
Reginald Lamar Bramber II <architekespy@Reginalds-MacBook-Pro.local>
2018-09-21 14:47:12 -0400

    one user story complete

--- routes/users.js
+++ routes/users.js
@@ -14,52 +14,52 @@ router.get('/', (req, res) => {
     })
  })
 
-// // NEW, RENDER NEW FORM
-// router.get('/new', (req, res) => {
-//     res.render('users/new')
-//   })
+// NEW, RENDER NEW FORM
+router.get('/new', (req, res) => {
+    res.render('users/new')
+  })
 
 
-// // SHOW, SHOW ONE
-// router.get('/:id', (req, res) => {
-//     User.findById(req.params.id)
-//       .then((user) => {
-//         res.render('users/show', { user })
-//       })
-//   })
+// SHOW, SHOW ONE
+router.get('/:id', (req, res) => {
+    User.findById(req.params.id)
+      .then((user) => {
+        res.render('users/show', { user })
+      })
+  })
 
-//   // EDIT, RENDER EDIT FORM
-// router.get('/:id/edit', (req, res) => {
-//     User.findById(req.params.id)
-//       .then((user) => {
-//         res.render('users/edit', { user })
-//       })
-//   })
+  // EDIT, RENDER EDIT FORM
+router.get('/:id/edit', (req, res) => {
+    User.findById(req.params.id)
+      .then((user) => {
+        res.render('users/edit', { user })
+      })
+  })
 
-//   // CREATE
-// router.post('/', (req, res) => {
-//     // const newUser = new User(req.body)
-//     // newUser.save()
-//     User.create(req.body)
-//       .then((user) => {
-//         res.redirect(`/users/${user._id}`)
-//       })
-//   })
+  // CREATE
+router.post('/', (req, res) => {
+    // const newUser = new User(req.body)
+    // newUser.save()
+    User.create(req.body)
+      .then((user) => {
+        res.redirect(`/users/${user._id}`)
+      })
+  })
 
-//   // UPDATE
-// router.put('/:id', (req, res) => {
-//     User.findByIdAndUpdate(req.params.id, req.body)
-//       .then((user) => {
-//         res.redirect(`/users/${user._id}`)
-//       })
-//   })
+  // UPDATE
+router.put('/:id', (req, res) => {
+    User.findByIdAndUpdate(req.params.id, req.body)
+      .then((user) => {
+        res.redirect(`/users/${user._id}`)
+      })
+  })
 
-//   // DELETE
-// router.delete('/:id', (req, res) => {
-//     User.findByIdAndRemove(req.params.id)
-//       .then(() => {
-//         res.redirect('/users')
-//       })
-//   })
+  // DELETE
+router.delete('/:id', (req, res) => {
+    User.findByIdAndRemove(req.params.id)
+      .then(() => {
+        res.redirect('/users')
+      })
+  })
 
 module.exports = router;
