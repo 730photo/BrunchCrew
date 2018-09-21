ac3770599f6d4e505cf50eb7f89ed6952303fbb8
Reginald Lamar Bramber II <architekespy@Reginalds-MacBook-Pro.local>
2018-09-21 09:29:38 -0400

    progress

--- routes/users.js
+++ routes/users.js
@@ -12,12 +12,51 @@ router.get('/', (req, res) => {
 })
 
 // NEW, RENDER NEW FORM
+router.get('/new', (req, res) => {
+    res.render('users/new')
+  })
 
 
 // SHOW, SHOW ONE
+router.get('/:id', (req, res) => {
+    User.findById(req.params.id)
+      .then((user) => {
+        res.render('users/show', { user })
+      })
+  })
 
+  // EDIT, RENDER EDIT FORM
+router.get('/:id/edit', (req, res) => {
+    User.findById(req.params.id)
+      .then((user) => {
+        res.render('users/edit', { user })
+      })
+  })
 
+  // CREATE
+router.post('/', (req, res) => {
+    // const newUser = new User(req.body)
+    // newUser.save()
+    User.create(req.body)
+      .then((user) => {
+        res.redirect(`/users/${user._id}`)
+      })
+  })
 
+  // UPDATE
+router.put('/:id', (req, res) => {
+    User.findByIdAndUpdate(req.params.id, req.body)
+      .then((user) => {
+        res.redirect(`/users/${user._id}`)
+      })
+  })
 
+  // DELETE
+router.delete('/:id', (req, res) => {
+    User.findByIdAndRemove(req.params.id)
+      .then(() => {
+        res.redirect('/users')
+      })
+  })
 
 module.exports = router;
